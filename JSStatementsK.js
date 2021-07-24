dbfb880d49aa8cc40e49f85beb097d4c97f05408
Krishna Adhikari <krishna.forkhaki@gmail.com>
2021-07-24 09:47:28 +0545

    statments completed

---
+++ JSStatementsK.js
@@ -0,0 +1,42 @@
+/*
+JS statements: 
+    JS statements are used to control the program flow 
+    Some examples of js statements are:
+    --> //comments
+    --> let
+    --> var
+    --> break
+    --> continue
+    --> for
+    --> for..in
+    --> functions
+    --> if..else
+    --> new
+    --> return
+    --> while and so on
+*/
+
+
+let full_name = "Krishna Adhikari";
+var hobby = "Programming & Coding";
+console.log("My name is: ", full_name, "\nand My hobby is: ", hobby);
+console.log();
+
+/*
+    JS Keywords
+    JS statements often start with a keyword
+    List of some of the keywords in JS are:
+
+    --> let     :       declares block variable
+    --> var     :       declares variable
+    --> const   :       declares a block constant
+    --> if      :       makes a block of statemtnts to be executed on a condition
+    --> switch  :       marks a block of statements to be executed in different cases
+*/
+
+
+//back tick in js is called template literals
+let checkName = "Krishna Adhikari";
+var hobby = "Programming & Coding";
+console.log(`My name is: ${checkName} and 
+My hobby is ${hobby}`);
