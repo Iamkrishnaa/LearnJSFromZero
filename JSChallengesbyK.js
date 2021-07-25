f3390d5b263d64ce2b4169a8ed3cb0fb71e0aade
Krishna Adhikari <krishna.forkhaki@gmail.com>
2021-07-25 22:54:46 +0545

    challenge accepted

--- JSChallengesbyK.js
+++ JSChallengesbyK.js
@@ -1,5 +1,5 @@
 /*
-challenge 1
+CHALLENGE 1:
     Given n, take the sum of the digits of n. 
     If that value has more than one digit, 
     continue reducing in this way until a 
@@ -12,5 +12,35 @@ challenge 1
             132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
             493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
     }
-
 */
+
+
+var num = 493193;
+console.log("The given number is:", num);
+numArr = [];
+
+
+while(true){
+    var numSum = getSum(num);
+    var numStr = numSum.toString();
+    var numLength = numStr.length;
+    num = numSum;
+    console.log(">>",num);
+    if(numLength == 1){
+        break;
+    }
+}
+
+function getSum(number){
+    var strNum = number.toString();
+    var numSize = strNum.length - 1;
+
+    var sum = 0;
+
+    while(numSize >= 0){
+        let num = parseInt(strNum[numSize]);
+        sum += num;
+        numSize--;
+    }
+    return sum;
+}
